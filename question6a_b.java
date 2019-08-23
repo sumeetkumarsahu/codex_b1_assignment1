eb26b54841220b9331a52791aa8bcec98e88e009
sumeetkumarsahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-23 12:45:45 +0530

    Create question6a_b.java

---
+++ question6a_b.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Question_6 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input= new Scanner(System.in);
+		System.out .print("Enter a Year to check it is leap year or not:");
+		int leap_year= input.nextInt();
+		  if (leap_year%4==0)
+		    { 
+			  if(leap_year%100==0)
+	         	{
+			      if(leap_year%400==0)
+			    	  System.out.println("Year is a leap year:"+leap_year);
+			    }
+	         }
+	   else
+		   System.out.println("Year is not a leap year:"+leap_year);
+	}
+     
+}
