453ef43366d47c2fac5254517de2e38eed36af4e
sumeetkumarsahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-23 12:40:15 +0530

    Create question2b.java

---
+++ question2b.java
@@ -0,0 +1,9 @@
+public class Question_2b 
+{
+	public static void main(String[] args)
+	{
+	int a=8;
+	System.out.println(a++ +","+ ++a +","+ a++);
+	System.out.println(--a +","+ a + ","+ a--);
+	}
+}
