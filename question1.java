b2fb695e124e1dd24347f5aec3aa668af87e82f0
sumeetkumarsahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-23 12:37:52 +0530

    Create question1.java

---
+++ question1.java
@@ -0,0 +1,13 @@
+public class Question_1 
+{
+
+	public static void main(String[] args) 
+	{
+		char ch='a';
+	 System.out.println(5/2);
+	 System.out.println(5/2.0);
+	 System.out.println((((int)ch+1)%2)==0);
+
+	}
+
+}
