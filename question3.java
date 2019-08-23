20397b31d90ab79f0af0e819bb72a97988151ad2
sumeetkumarsahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-23 12:41:21 +0530

    Create question3.java

---
+++ question3.java
@@ -0,0 +1,14 @@
+public class Question_3 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=7, b=9;
+		System.out.println(a&b); //and
+		System.out.println(a|b); //or
+		System.out.println(a^b); //xor
+		System.out.println(a<<2); //left shift
+		System.out.println(a>>2); //right shift
+	}
+
+}
