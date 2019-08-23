596e304b06a8063844f0bea510b323edffe685ae
sumeetkumarsahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-23 12:42:30 +0530

    Create question4.java

---
+++ question4.java
@@ -0,0 +1,15 @@
+public class Question_4 
+{
+
+	public static void main(String[] args) 
+	{ 
+		int grt1,grt2, temp;
+		int a=10, b=2, c=1, d=9;
+		temp=a>b?a:b;
+		grt1=temp>c?temp:c;
+		grt2=grt1>d?grt1:d;
+		System.out.println(grt2);
+
+	}
+
+}
