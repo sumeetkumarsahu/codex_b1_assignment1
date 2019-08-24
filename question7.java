26ff519eec90e7fa89824971b9eae7ff07515a05
sumeetkumarsahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-24 14:50:08 +0530

    Update question7.java

--- question7.java
+++ question7.java
@@ -8,8 +8,7 @@ public class Question_7
 	System.out.println("Enter two nos");
 	int no1= input.nextInt();
 	int no2= input.nextInt();
-	Scanner input2= new Scanner(System.in);
-	System.out.println("Enter the '1for+','2for-','3for*','4for/','5for^'");
+	System.out.println("Enter the '1for+','2for-','3for*','4for/'");
 	int operation= input.nextInt();
 	
 	switch(operation)
@@ -18,7 +17,6 @@ public class Question_7
 	case 2:System.out.println("Answer for subtraction is:"+(no1-no2));break;
 	case 3:System.out.println("Answer for multiplication is:"+(no1*no2));break;
 	case 4:System.out.println("Answer for division is:"+(no1/no2));break;
-	case 5:System.out.println("Answer for power is:"+(no1^no2));break;
 	default:System.out.println("Entered Invalid operation");
 	
 	}
