c789e540ae0662bb72a7deeeb185a5bc29d20837
sumeetkumarsahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-24 16:00:16 +0530

    Update question4.java

--- question4.java
+++ question4.java
@@ -3,12 +3,10 @@ public class Question_4
 
 	public static void main(String[] args) 
 	{ 
-		int grt1,grt2, temp;
+		int grt_no;
 		int a=10, b=2, c=1, d=9;
-		temp=a>b?a:b;
-		grt1=temp>c?temp:c;
-		grt2=grt1>d?grt1:d;
-		System.out.println(grt2);
+		grt_no=((a>b?a:b)>c?(a>b?a:b):c)>d?((a>b?a:b)>c?(a>b?a:b):c):d;
+		System.out.println(grt_no);
 
 	}
 
