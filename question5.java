6a3265317f85dc9b82a40b149a6fe4f666f91642
Sumeet Kumar Sahu <53472581+sumeetkumarsahu@users.noreply.github.com>
2019-08-25 22:54:50 +0530

    Update question5.java

--- question5.java
+++ question5.java
@@ -12,18 +12,20 @@ public class Question_5
 		if((sal_rn>0.00)&&(sal_rn<=14999.99))
 		System.out.println("tax due="+ (0.00+(sal_rn-0.00)*0.18));
 		
-		if ((sal_rn>=15000.00)&&(sal_rn<=29999.00))
-        System.out.println("tax due="+ (2250.00+(sal_rn-15000.00)*0.18));
+		else if ((sal_rn>=15000.00)&&(sal_rn<=29999.00))
+                System.out.println("tax due="+ (2250.00+(sal_rn-15000.00)*0.18));
         
-        if((sal_rn>=30000.00)&&(sal_rn<=49999.99))
+                else if((sal_rn>=30000.00)&&(sal_rn<=49999.99))
 		System.out.println("tax due="+ (5400.00+(sal_rn-30000.00)*0.22));
 		
-		if((sal_rn>=50000.00)&&(sal_rn<=79999.00))
-        System.out.println("tax due="+ (11000.00+(sal_rn-50000.00)*0.27));
+		else if((sal_rn>=50000.00)&&(sal_rn<=79999.00))
+                System.out.println("tax due="+ (11000.00+(sal_rn-50000.00)*0.27));
         
-        if((sal_rn>=80000.00)&&(sal_rn<=150000.00))
+                else if((sal_rn>=80000.00)&&(sal_rn<=150000.00))
 		System.out.println("tax due="+ (21600.00+(sal_rn-80000.00)*0.33));
 		
+		else
+                System.out.println("tax due=" +(-1.0));
 	}
 
 }
